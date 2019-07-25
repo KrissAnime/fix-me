cdc4b79ebed0602d849f16063ec6bbe578cf905f
Crescendo BESTER <cbester@c4r6s3.wethinkcode.co.za>
2019-07-25 13:17:20 +0200

    Managed to connect server and broker using new system, messages can get back to broker from the server, need to begin FIX message class to unmarshall and marshall the messages correctly, afterwards will need to account for multiple brokers

--- fixme-broker/src/main/java/wethinkcode/com/fixme/broker/BrokerClient.java
+++ fixme-broker/src/main/java/wethinkcode/com/fixme/broker/BrokerClient.java
@@ -1,26 +1,42 @@
 package wethinkcode.com.fixme.broker;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
 
 public class BrokerClient {
+    private static SocketChannel client;
+    private static ByteBuffer buffer;
+    private static BrokerClient broker = new BrokerClient();
+
     public static void main(String args[]) {
         try {
-//            ServerSocket brokerListener = new ServerSocket(4000);
-            Socket broker = new Socket("localhost", 5000);
-            InputStreamReader inputStreamReader = new InputStreamReader(broker.getInputStream());
-            BufferedReader buff = new BufferedReader(inputStreamReader);
+            client = SocketChannel.open(new InetSocketAddress("localhost",5000));
+            buffer = ByteBuffer.allocate(2048);
 
-            PrintWriter printWriter = new PrintWriter(broker.getOutputStream());
-            printWriter.print("Client connection");
-            printWriter.flush();
+            while (true) {
+                // Sending the message to the server;
+                buffer = ByteBuffer.wrap("I am the broker".getBytes());
+                client.write(buffer);
+                buffer.flip();
+                buffer.clear();
 
-//            Socket routerResponse = brokerListener.accept();
-            System.out.println("Awaiting connection");
-
-            System.out.println("Router response " + buff.readLine());
+                //Reading the response from the server
+                client.read(buffer);
+                System.out.println("Server response " + new String(buffer.array()).trim());
+                buffer.flip();
+                buffer.clear();
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
 
         } catch (UnknownHostException e) {
             System.out.println("Broker attempting to connect to unknown host");
--- fixme-router/src/main/java/wethinkcode/com/fixme/router/Server.java
+++ fixme-router/src/main/java/wethinkcode/com/fixme/router/Server.java
@@ -1,14 +1,16 @@
 package wethinkcode.com.fixme.router;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Server {
 
@@ -19,14 +21,31 @@ public class Server {
         /*******TESTING WINDOW*********/
 
         try {
-            ServerSocketChannel server = ServerSocketChannel.open().bind(new InetSocketAddress(4000));
+            System.out.println("Starting up Server...");
             Selector selector = Selector.open();
+            ServerSocketChannel server = ServerSocketChannel.open().bind(new InetSocketAddress("localhost", 5000));
+            ByteBuffer buffer = ByteBuffer.allocate(2048);
+            server.configureBlocking(false);
+            server.register(selector, SelectionKey.OP_ACCEPT);
 
+            System.out.println("Waiting for client connection");
             while (true) {
-                SocketChannel client = server.accept();
-                client.configureBlocking(false);
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
 
-                client.register(selector, SelectionKey.OP_CONNECT);
+                    if (key.isAcceptable()) {
+                        registerClient(selector, server);
+                    }
+
+                    if (key.isReadable()) {
+                        answerClient(buffer, key);
+                    }
+
+                    iterator.remove();
+                }
             }
         } catch (IOException e) {
             System.out.println("The selector had in IOException");
@@ -36,5 +55,29 @@ public class Server {
         /*******TESTING WINDOW*********/
         /*******TESTING WINDOW*********/
         /*******TESTING WINDOW*********/
+    }
+
+    private static void registerClient(Selector selector, ServerSocketChannel server) throws IOException {
+        SocketChannel client = server.accept();
+        client.configureBlocking(false);
+        client.register(selector, SelectionKey.OP_READ);
+    }
+
+    private static void answerClient(ByteBuffer buffer, SelectionKey key) throws IOException {
+        SocketChannel client = (SocketChannel)key.channel();
+        client.read(buffer);
+        if (new String(buffer.array()).trim().equals("Exit")) {
+            client.close();
+            System.out.println("Disconnecting from the client");
+        } else {
+            System.out.println("Client says " + new String(buffer.array()).trim());
+        }
+        buffer.flip();
+        buffer.clear();
+
+        buffer = ByteBuffer.wrap("I am the server".getBytes());
+        client.write(buffer);
+        buffer.flip();
+        buffer.clear();
     }
 }
