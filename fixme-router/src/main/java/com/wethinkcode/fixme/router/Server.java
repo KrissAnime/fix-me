9f2ebdb2b6ee370144d229cd3c9070e4992c5773
Crescendo BESTER <cbester@c5r9s4.wethinkcode.co.za>
2019-08-08 11:05:11 +0200

    Built message dispatcher and reworked server

--- fixme-router/src/main/java/com/wethinkcode/fixme/router/Server.java
+++ fixme-router/src/main/java/com/wethinkcode/fixme/router/Server.java
@@ -1,7 +1,7 @@
 package com.wethinkcode.fixme.router;
 
 import com.wethinkcode.fixme.router.models.FIXMessage;
-import sun.jvm.hotspot.oops.Mark;
+import com.wethinkcode.fixme.router.models.MessageDispatcher;
 
 import java.io.*;
 import java.net.InetAddress;
@@ -11,19 +11,11 @@ import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Server {
     static int brokerID = 99999;
-    enum BufferState {
-        READ,
-        WRITE;
-    }
-    static BufferState state = BufferState.READ;
-    static HashMap<SocketAddress, FIXMessage> fixMessageHashMap = new HashMap<>();
+
     static final String[] randomResponses = {
             "This is the server",
             "Server system logs can be accessed via log file",
@@ -49,136 +41,21 @@ public class Server {
             new Thread(brokerServer).start();
             new Thread(marketServer).start();
 
-//            marketThread.run();
-//            brokerThread.run();
-//            Sleep(3);
-
-
-//            System.out.println("Starting server");
-//            AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(group).bind(new InetSocketAddress(ports));
-//            server = AsynchronousServerSocketChannel.open(group).bind(new InetSocketAddress(5001));
-            //            AsynchronousChannelGroup asynchronousChannelGroup = AsynchronousChannelGroup.withFixedThreadPool(2, );
-//            server.bind(new InetSocketAddress(5001));
-//            while (true) {
-//                AsynchronousSocketChannel channel = server.accept().get();
-//            }
-//            System.out.println(server.getLocalAddress());
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        try {
-//
-//            System.out.println("Starting up Server...");
-//            Selector selector = Selector.open();
-//            ServerSocketChannel server = ServerSocketChannel.open().bind(new InetSocketAddress("localhost", 5000));
-//            server.bind(new InetSocketAddress(5001));
-//            ByteBuffer buffer = ByteBuffer.allocate(2048);
-//            server.configureBlocking(false);
-//            server.register(selector, SelectionKey.OP_ACCEPT);
-//
-//            System.out.println("Waiting for client connection");
-//            while (true) {
-//                selector.select();
-//                Set<SelectionKey> selectionKeys = selector.selectedKeys();
-//                Iterator<SelectionKey> iterator = selectionKeys.iterator();
-//                while (iterator.hasNext()) {
-//                    buffer.clear();
-//                    SelectionKey key = iterator.next();
-//
-//                    if (key.isAcceptable()) {
-//                        registerClient(selector, server);
-//                    }
-//
-//                    if (key.isReadable()) {
-//                        readClientMessage(buffer, key);
-//                    }
-//
-//                    if (key.isWritable()) {
-//                        answerClient(buffer, key);
-//                    }
-//
-//                    iterator.remove();
-//                }
-//                Sleep(2);
-//            }
-//        } catch (IOException e) {
-//            System.out.println("The selector had in IOException");
-//            e.printStackTrace();
-//        }
 
         /*******TESTING WINDOW*********/
         /*******TESTING WINDOW*********/
         /*******TESTING WINDOW*********/
     }
 
-    private static void registerClient(Selector selector, ServerSocketChannel server) throws IOException {
-        SocketChannel client = server.accept();
-        int interestSet = SelectionKey.OP_READ | SelectionKey.OP_WRITE;
-        System.out.println("Client Connected " + client.getRemoteAddress());
-        client.configureBlocking(false);
-        client.register(selector, interestSet, client.getRemoteAddress());
-        Sleep(1);
-    }
-
-    private static void readClientMessage(ByteBuffer buffer, SelectionKey key) throws IOException {
-        SocketChannel client = (SocketChannel)key.channel();
-        System.out.println("Reading client Message  of " + key.attachment().toString());
-//        System.out.println("Client says before clear " + new String(buffer.array()).trim());
-        buffer = ClearBuffer(buffer);
-        if (state != BufferState.READ) {
-            buffer.flip();
-            state = BufferState.READ;
-        }
-        client.read(buffer);
-        if (new String(buffer.array()).trim().equals("Exit")) {
-            System.out.println("Disconnecting from the client with ID " + key.attachment());
-            client.close();
-        } else {
-            FIXMessage fixMessage = new FIXMessage(new String(buffer.array()).trim());
-            fixMessageHashMap.putIfAbsent(client.getRemoteAddress(), fixMessage);
-            System.out.println("Client says " + new String(buffer.array()).trim());
-        }
-//        ClearBuffer(buffer);
-//        Sleep(2);
-    }
-
-    private static void answerClient(ByteBuffer buffer, SelectionKey key) throws IOException {
-        SocketChannel client = (SocketChannel)key.channel();
-        if (fixMessageHashMap.containsKey(client.getRemoteAddress())) {
-//            System.out.println("Before answer clearing " + new String(buffer.array()).trim());
-            buffer = ClearBuffer(buffer);
-            if (state != BufferState.WRITE) {
-                buffer.flip();
-                state = BufferState.WRITE;
-            }
-//            System.out.println("After answer clearing " + new String(buffer.array()).trim());
-//        System.out.println("Send client message");
-//        System.out.println("Stringed buffer " + new String(buffer.array()).trim());
-            buffer = ByteBuffer.wrap(randomResponses[ThreadLocalRandom.current().nextInt(0, randomResponses.length)].getBytes());
-            client.write(buffer);
-        }
-//        System.out.println(fixMessageHashMap.toString());
-//        ClearBuffer(buffer);
-//        Sleep(2);
-    }
-
-    private static void Sleep(long time) {
-        try {
-            TimeUnit.SECONDS.sleep(time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private static void Sleep(long seconds) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(seconds);
     }
 
     private static int setBrokerID() {
         return ++brokerID;
-    }
-
-    private static ByteBuffer ClearBuffer(ByteBuffer buffer) {
-        buffer.clear();
-        buffer.put(new byte[2048]);
-        buffer.clear();
-        return buffer;
     }
 }
 
@@ -186,6 +63,7 @@ class BrokerServer implements Runnable {
     AsynchronousChannelGroup group;
     ByteBuffer buffer = ByteBuffer.allocate(2048);
     List<SocketAddress> clientAddresses;
+    MessageDispatcher messageDispatcher = new MessageDispatcher();
 
     public BrokerServer(AsynchronousChannelGroup group, List<SocketAddress> clientAddresses) {
         this.group = group;
@@ -205,25 +83,56 @@ class BrokerServer implements Runnable {
                 public void completed(AsynchronousSocketChannel clientSocket, Object attachment) {
                     synchronized (clientAddresses) {
                         try {
-                            if (!clientAddresses.contains(clientSocket.getLocalAddress())) {
-                                clientAddresses.add(clientSocket.getLocalAddress());
-                                System.out.println("Adding client " + clientSocket.getRemoteAddress().toString());
-                                buffer.flip();
-                                clientSocket.write(ByteBuffer.wrap(clientSocket.getRemoteAddress().toString().getBytes()));
-                                buffer = ClearBuffer(buffer);
-                                buffer.flip();
-
+//                            if (!clientAddresses.contains(clientSocket.getLocalAddress())) {
+//                                clientAddresses.add(clientSocket.getLocalAddress());
+//                                System.out.println("Adding client " + clientSocket.getRemoteAddress().toString());
 //                                buffer.flip();
-//                                clientSocket.write(ByteBuffer.wrap());
-                            } else {
-                                clientSocket.read(buffer);
-                                buffer.flip();
-                                buffer = ClearBuffer(buffer);
-                                System.out.println("Broker Buffer " + new String(buffer.array()).trim());
-                                System.out.println("Broker attachment" + attachment);
-                                clientSocket.write(ByteBuffer.wrap("This is the server to broker".getBytes()));
-                            }
+//                                Future<Integer> future = clientSocket.write(ByteBuffer.wrap(clientSocket.getRemoteAddress().toString().getBytes()));
+//
+//                                while (!future.isDone()) {
+//                                    System.out.println("Waiting for message to be send to client...");
+//                                    Thread.sleep(100);
+//                                }
+//
+////                                buffer = ClearBuffer(buffer);
+////                                buffer.flip();
+//
+////                                buffer.flip();
+////                                clientSocket.write(ByteBuffer.wrap());
+//                            } else {
+
+                                Future<Integer> future = clientSocket.read(buffer);
+
+
+
+                                while (!future.isDone()) {
+                                    System.out.println("Waiting for broker message...");
+                                    Thread.sleep(250);
+                                }
+                                FIXMessage fixMessage = new FIXMessage(new String(buffer.array()).trim());
+
+                                messageDispatcher.AddBrokerAddress(fixMessage.getRoutingSenderID(), clientSocket);
+
+                                fixMessage = messageDispatcher.SendMessageToMarket(fixMessage);
+
+                                messageDispatcher.SendMessageToBroker(fixMessage);
+//                                buffer.flip();
+//                                buffer = ClearBuffer(buffer);
+//                                System.out.println("Broker Buffer " + new String(buffer.array()).trim());
+//                                System.out.println("Broker attachment" + attachment);
+//                                future = clientSocket.write(ByteBuffer.wrap(fixMessage.MarshallMessage().getBytes()));
+
+
+
+                                while (!future.isDone()) {
+                                    System.out.println("Waiting for FIX message to be sent back to broker...");
+                                    Thread.sleep(200);
+                                }
+
+//                            }
                         } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
@@ -247,6 +156,7 @@ class BrokerServer implements Runnable {
             e.printStackTrace();
         }
     }
+
     private static ByteBuffer ClearBuffer(ByteBuffer buffer) {
 //        System.out.println("Before clear buffer" + new String(buffer.array()).trim());
         buffer.clear();
--- fixme-router/src/main/java/com/wethinkcode/fixme/router/models/MessageDispatcher.java
+++ fixme-router/src/main/java/com/wethinkcode/fixme/router/models/MessageDispatcher.java
@@ -1,4 +1,60 @@
 package com.wethinkcode.fixme.router.models;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Future;
+
 public class MessageDispatcher {
+    Map<String, AsynchronousSocketChannel> marketAddresses = new HashMap<>();
+    Map<String, AsynchronousSocketChannel> brokerAddresses = new HashMap<>();
+
+    public MessageDispatcher(){}
+
+    public void AddMarketAddress(String address, AsynchronousSocketChannel market) {
+        if (!marketAddresses.containsValue(market)){
+            marketAddresses.put(address, market);
+        }
+    }
+
+    public void RemoveMarketAddress(String address) throws IOException {
+        marketAddresses.get(address).close();
+        marketAddresses.remove(address);
+    }
+
+    public void AddBrokerAddress(String address, AsynchronousSocketChannel market) { brokerAddresses.put(address, market); }
+
+    public void RemoveBrokerAddress(String address) throws IOException {
+        brokerAddresses.get(address).close();
+        brokerAddresses.remove(address);
+    }
+
+    public FIXMessage SendMessageToMarket(FIXMessage fixMessage) throws IOException, InterruptedException {
+        System.out.println("Sending message to market");
+        ByteBuffer buffer = ByteBuffer.allocate(2048);
+
+        AsynchronousSocketChannel destination = marketAddresses.get(fixMessage.getRoutingReceiverID());
+        destination.write(ByteBuffer.wrap(fixMessage.MarshallMessage().getBytes()));
+
+        Future<Integer> result = destination.read(buffer);
+        while (!result.isDone()) {
+            System.out.println("Waiting on response from market... " + destination.getRemoteAddress());
+            Thread.sleep(200);
+        }
+        System.out.println("Message received from the market...\t" + new String(buffer.array()).trim());
+        return new FIXMessage(new String(buffer.array()).trim());
+    }
+
+    public void SendMessageToBroker(FIXMessage fixMessage) throws InterruptedException {
+        AsynchronousSocketChannel destination = brokerAddresses.get(fixMessage.getRoutingReceiverID());
+        Future<Integer> future = destination.write(ByteBuffer.wrap(fixMessage.MarshallMessage().getBytes()));
+
+        while (!future.isDone()) {
+            System.out.println("Attempting to send message to the broker...");
+            Thread.sleep(200);
+        }
+        System.out.println("Message was successfully sent to the broker");
+    }
 }
